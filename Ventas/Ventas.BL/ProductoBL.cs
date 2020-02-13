14650db99c0e6691d221042530f725c22c807057
wilmer-cloud <enamoradomolina@gmail.com>
2020-02-12 22:33:01 -0600

    Ingresando ventas

---
+++ Ventas/Ventas.BL/ProductoBL.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ventas.BL
+{
+    public class ProductoBL
+    {
+        public  List<Producto>  ListadeProductos { get; set; }
+
+        public ProductoBL()
+        {
+            ListadeProductos = new List<Producto>();
+            Producto producto1 = new Producto();
+            producto1.Id = 1;
+            producto1.Descripcion = "iphone";
+            Producto producto2 = new Producto();
+            producto2.Id = 2;
+            producto2.Descripcion = "Huawei P30";
+
+            
+
+            ListadeProductos.Add(producto1);
+            ListadeProductos.Add(producto2);
+            
+
+        }
+
+        public  void    agregar(int id, string descripcion)
+        {
+            var producto = new Producto();
+            producto.Id = id;
+            producto.Descripcion = descripcion;
+
+            ListadeProductos.Add(producto);
+
+            
+        }
+    }
+}
---
+++ Ventas/Ventas.Win/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Ventas.BL;
+
+namespace Ventas.Win
+{
+    public partial class Form1 : Form
+    {
+        ProductoBL productosBL;
+        public Form1()
+        {
+            InitializeComponent();
+
+            productosBL = new ProductoBL();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+
+            dataGridView1.DataSource = productosBL.ListadeProductos;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var id = int.Parse(textBox1.Text);
+            var descripcion = textBox2.Text;
+
+            productosBL.agregar(3, "Huawei P30");
+
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = productosBL.ListadeProductos;
+                
+        }
+    }
+}
